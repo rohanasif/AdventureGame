7455c1ed175708e450dbb02713a240a867f589fe
rohan <rohanasif@yahoo.com>
2022-10-03 21:36:08 +0500

    add functions

--- adventure_game.py
+++ adventure_game.py
@@ -1,13 +1,16 @@
 import time
 
-print("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
-time.sleep(1)
-print("Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village.")
-time.sleep(1)
-print("Enter 1 to knock on the door of the house.")
-time.sleep(1)
-print("Enter 2 to peer into the cave.")
-time.sleep(1)
+
+def print_pause(msg):
+    print(msg)
+    time.sleep(1)
+
+
+print_pause("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
+print_pause("Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village.")
+print_pause("Enter 1 to knock on the door of the house.")
+print_pause("Enter 2 to peer into the cave.")
+
 
 while True:
     choice = input("What would you like to do?")
